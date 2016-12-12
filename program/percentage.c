e4abe9b5361ccb4f9f0c2e4330590c587726dc6a
sanjay2310 <sanjay.rana070@gmail.com>
2016-12-12 22:25:00 +0530

    sanjay

---
+++ program/percentage.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	float f;
+	printf("enter marks of english\n");
+	scanf("%d",&a);
+	 printf("enter marks of hindi\n");
+	  scanf("%d",&b);
+	  printf("enter marks of maths\n");
+	  scanf("%d",&c);
+	     printf("enter marks of sst\n");
+	     scanf("%d",&d);
+	     printf("enter marks of science\n");
+	     scanf("%d",&e);
+	     f = (a+b+c+d+e)*100/500;
+	     printf("total percentage = %f\n",f);
+	     return 0;
+}
+
+
