83b350197d6eedcd51e53ec39d35fff0f50e42b2
sanjay2310 <sanjay.rana070@gmail.com>
2016-12-13 20:23:01 +0530

    first even or odd  program

---
+++ program/evenandodd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the program\n");
+	scanf("%d",&a);
+	if(a%2==0)
+		printf("Is even number \n");
+		else
+			printf("is odd number\n");
+			return 0;
+}
+
