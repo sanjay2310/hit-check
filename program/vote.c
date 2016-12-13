227ec44828fb78204d337143a533758c999d2e5c
sanjay2310 <sanjay.rana070@gmail.com>
2016-12-14 00:22:21 +0530

    vote

---
+++ program/vote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter your age \n");
+	scanf("%d",&a);
+	if(a>=18)
+		printf("you are ready to vote \n");
+		else
+			printf("you not ready to vote\n");
+			return 0;
+}
