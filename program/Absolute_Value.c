eb6f305c3fb446b9a96e6459bb874c6ceba96e3e
sanjay2310 <sanjay.rana070@gmail.com>
2016-12-21 20:13:58 +0530

    Absolute_Value.c

---
+++ program/Absolute_Value.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	printf("enter the number\n");
+	scanf("%d",&a);
+	printf("enter the number \n");
+	scanf("%d",&b);
+	c = abs(a);
+	d = abs(b);
+	printf("the Absolute Value is = %d\n",c);
+	 printf("the Absolute Value is = %d\n",d);
+	 return 0;
+}
