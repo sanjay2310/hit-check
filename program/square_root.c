cf37046161edb92ef4f3a3acfd6ea2313b6c845e
sanjay2310 <sanjay.rana070@gmail.com>
2016-12-21 23:39:48 +0530

    square root

---
+++ program/square_root.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a;
+	int b;
+	printf("enter the number\n");
+	scanf("%d",&a);
+	b = sqrt(a);
+	printf("the square root = %d\n",b);
+	return 0;
+}
