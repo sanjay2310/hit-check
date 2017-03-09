3adc5cea7b19482b0c85afede0630b70a87ab9fd
Sanjay Prakash Rana <sanjay.ran070@gmail.com>
2017-03-09 20:19:01 +0530

    function table and name

---
+++ program/funtion.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void message()
+{
+	printf("RB rana \n");
+}
+
+void Table(int g)
+{
+	message();
+	int a;
+	for(a = 1; a < = 10; a++)
+		printf("%d\n", a*g);
+}
+
+int main()
+{
+	 int c;
+	 scanf("%d", &c);
+	 printf("sanjay.rana\n");
+	 Table(c);
+	 return 0;
+}
+
+
