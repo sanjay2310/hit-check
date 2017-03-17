73900bececbdaf98fbaffbf5c6bfc02a327db54c
Sanjay Prakash Rana <sanjay.ran070@gmail.com>
2017-03-17 15:40:15 +0530

    figure

---
+++ program/rig_tir.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	int b;
+	int c;
+	scanf("%d",&c);
+
+	for(a = 1;a < = c;a++)
+	
+	{
+
+		for(b = 1;b < = c;b++)
+		{
+						printf("x");
+		}
+		printf("\n");
+	}
+	return 0;
+}
